0bc3fd5dce649d2d4a1421832b528a6d1492c7fa
akabynda <st084331@student.spbu.ru>
2025-04-23 04:15:32 +0300

    Chapin Q

---
+++ metrics/chapin_q.py
@@ -0,0 +1,34 @@
+from tree_sitter import Node
+
+from metrics import metric
+
+_READ = {"identifier"}
+_WRITE = {"assignment_expression", "var_modifier", "property_declaration"}
+_CTRL = {"if_expression", "when_expression", "while_statement", "do_while_statement", "for_statement"}
+
+
+@metric("Chapin Q")
+def chapin_q(root: Node):
+    P = M = C = T = 0
+    declared = set()
+    used = {}
+
+    def _scan(n: Node):
+        nonlocal P, M, C
+        if n.type in _WRITE:
+            name = n.children[0].text if n.children else None
+            if name:
+                declared.add(name)
+                M += 1
+        if n.type in _READ:
+            used.setdefault(n.text, 0)
+            used[n.text] += 1
+        if n.type in _CTRL:
+            C += 1
+        for c in n.children:
+            _scan(c)
+
+    _scan(root)
+    P = len([k for k in used if k not in declared])
+    T = max(len(used) - P - M, 0)
+    return P + 2 * M + 3 * C + 0.5 * T
