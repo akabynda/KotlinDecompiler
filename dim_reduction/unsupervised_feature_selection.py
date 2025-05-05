1300cc05205608d12b5ae80037ccbda17dcaee08
akabynda <st084331@student.spbu.ru>
2025-05-05 21:01:55 +0300

    personal info delete

--- dim_reduction/unsupervised_feature_selection.py
+++ dim_reduction/unsupervised_feature_selection.py
@@ -20,5 +20,3 @@ if __name__ == "__main__":
         print("Окончательно удалены как избыточные:", len(removed), removed)
 
     print("Остались:", len(reduced_df.columns), reduced_df.columns.tolist())
-
-"""Остались: 16 ['Abrupt Control Flow', 'Halstead Difficulty', 'Halstead Volume', 'Labeled Blocks', 'Pivovarsky N(G)', 'CE', 'PPL', 'CondE', 'LM_CE', 'LM_PPL', 'detekt_FunctionParameterNaming', 'detekt_MagicNumber', 'detekt_MaxLineLength', 'detekt_LoopWithTooManyJumpStatements', 'detekt_ClassNaming', 'detekt_InvalidPackageDeclaration']"""
