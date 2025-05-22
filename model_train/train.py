748117c34eb6879806277acc7d01141a48e194a4
st084331 <st084331@student.spbu.ru>
2025-05-22 04:45:28 +0300

    auto

--- model_train/train.py
+++ model_train/train.py
@@ -74,7 +74,7 @@ model = AutoModelForCausalLM.from_pretrained(
             bnb_4bit_compute_dtype=torch.float16
         ),
         trust_remote_code=True,
-        device_map={"": torch.cuda.current_device()},
+        device_map="auto",
         use_cache=False,
     )
 
